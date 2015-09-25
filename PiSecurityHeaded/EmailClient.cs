87b8738492bec0ba39ddbb0a7b9f5cb3bfb43d90
Ming Cheuk <ming.cheuk@spark64.com>
2015-09-25 17:22:30 +1200

    - Initial commit with projects added.

---
+++ PiSecurityHeaded/EmailClient.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiSecurityHeaded
+{
+    public sealed class EmailClient
+    {
+        const string DOMAIN = "sandbox3dda5b8dc9dd48cbbc402d3e4b38d126.mailgun.org";
+        const string API_KEY = "key-b56695ac74d09597f2e001a4f3967cdf";
+
+        public void SendMail()
+        {
+            SendMailAsync().Wait();
+        }
+
+        private async Task SendMailAsync()
+        {
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(UTF8Encoding.UTF8.GetBytes("api" + ":" + API_KEY)));
+
+
+            var form = new Dictionary<string, string>();
+            form["from"] = "intruders@gtfo.com";
+            form["to"] = "ming.cheuk@studentpartner.com";
+            form["subject"] = "Intruder Alert";
+            form["text"] = "Intruder detected at "+DateTime.Now.ToString();
+
+            var response = await client.PostAsync("https://api.mailgun.net/v2/" + DOMAIN + "/messages", new FormUrlEncodedContent(form));
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                Debug.WriteLine("Success");
+            }
+            else
+            {
+                Debug.WriteLine("StatusCode: " + response.StatusCode);
+                Debug.WriteLine("ReasonPhrase: " + response.ReasonPhrase);
+            }
+        }
+    }
+}
---
+++ PiSecurityHeaded/MainPage.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Gpio;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace PiSecurityHeaded
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private GpioPinValue value = GpioPinValue.Low;
+        private const int sensorPin = 26;
+        private GpioPin pin;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            InitGPIO();
+            pin.ValueChanged += Pin_ValueChanged;
+        }
+
+        private void Pin_ValueChanged(GpioPin sender, GpioPinValueChangedEventArgs args)
+        {
+            if (args.Edge == GpioPinEdge.RisingEdge)
+            {
+                var client = new EmailClient();
+                client.SendMail();
+
+                try
+                {
+                    playRecordedAudio();
+                }
+                catch (Exception e)
+                {
+                    Debug.WriteLine(e);
+                }
+            }
+        }
+
+        private void InitGPIO()
+        {
+            pin = GpioController.GetDefault().OpenPin(sensorPin);
+            pin.SetDriveMode(GpioPinDriveMode.Input);
+        }
+
+        private async void playRecordedAudio()
+        {
+            string CountriesFile = @"Assets\TornadoSiren.mp3";
+            StorageFolder InstallationFolder = Windows.ApplicationModel.Package.Current.InstalledLocation;
+            StorageFile file = await InstallationFolder.GetFileAsync(CountriesFile);
+            var stream = await file.OpenAsync(FileAccessMode.Read);
+
+            await this.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () => {
+                if (null != file)
+                {
+                    mediaElement.SetSource(stream, file.ContentType);
+                    mediaElement.Play();
+                }
+            });
+
+        }
+    }
+}
